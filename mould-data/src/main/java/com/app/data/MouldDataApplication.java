e4e15c1469d83c67bc33d790fd6d323dab595fa0
wangj <wangj@13322.com>
2018-03-20 17:41:47 +0800

    添加mybatis

--- mould-data/src/main/java/com/app/data/MouldDataApplication.java
+++ mould-data/src/main/java/com/app/data/MouldDataApplication.java
@@ -1,9 +1,11 @@
 package com.app.data;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.app.data.dao")
 public class MouldDataApplication {
 
 	public static void main(String[] args) {
--- mould-data/src/test/java/com/app/data/MouldDataApplicationTests.java
+++ mould-data/src/test/java/com/app/data/MouldDataApplicationTests.java
@@ -1,16 +1,29 @@
 package com.app.data;
 
+import com.alibaba.fastjson.JSON;
+import com.app.data.dao.UserMapper;
+import com.app.data.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MouldDataApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void test(){
+        System.out.println("=====================");
+        List<User> users = userMapper.selectAll();
+        System.out.println(JSON.toJSONString(users));
+        System.out.println("=====================");
+    }
 }
