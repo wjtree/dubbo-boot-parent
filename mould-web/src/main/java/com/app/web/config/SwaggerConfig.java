665c3adc20d037cda680c972fa218c5ec18f7322
wangj <wangj@13322.com>
2018-03-27 16:49:43 +0800

    修复 swagger2 配置导致无法显示 icon

--- mould-web/src/main/java/com/app/web/config/SwaggerConfig.java
+++ mould-web/src/main/java/com/app/web/config/SwaggerConfig.java
@@ -3,9 +3,6 @@ package com.app.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -14,9 +11,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
 @ComponentScan(basePackages = "com.app.web.controller")
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
     private static final String TITLE = "接口说明文档"; // 接口文档说明
     private static final String VERSION = "1.0"; // 接口文档版本
 
@@ -45,15 +41,5 @@ public class SwaggerConfig implements WebMvcConfigurer {
          * 文档协议 licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
          */
         return new ApiInfoBuilder().title(TITLE).version(VERSION).build();
-    }
-
-    /**
-     * 启用 Swagger UI静态资源访问
-     *
-     * @param registry
-     */
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
