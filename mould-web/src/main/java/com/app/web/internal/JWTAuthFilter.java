22513f523c3e3744918944c0db51b5b3e735bc80
wangj <wangj@13322.com>
2018-03-31 18:19:07 +0800

    修复 swagger2 配置导致无法显示 icon

--- mould-web/src/main/java/com/app/web/internal/JWTAuthFilter.java
+++ mould-web/src/main/java/com/app/web/internal/JWTAuthFilter.java
@@ -1,44 +1,47 @@
-//package com.app.web.internal;
-//
-//import com.app.core.util.JwtUtil;
-//import lombok.extern.log4j.Log4j2;
-//import org.apache.commons.lang3.StringUtils;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * 校验 JWT Token
-// */
-//@Log4j2
-//public class JWTAuthFilter extends BasicAuthenticationFilter {
-//
-//    public JWTAuthFilter(AuthenticationManager authenticationManager) {
-//        super(authenticationManager);
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        String header = request.getHeader("Authorization");
-//
-//        if (StringUtils.isBlank(header) || !StringUtils.startsWith(header, "Bearer ")) {
-//            log.warn("用户认证失败，请求头中 Authorization 字段不存在或格式错误");
-//            chain.doFilter(request, response);
-//            return;
-//        }
-//
-//        // 从 Token 中获取用户信息
-//        String userId = JwtUtil.parseJWT(header).getSubject();
-//        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, null);
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//        chain.doFilter(request, response);
-//    }
-//}
+package com.app.web.internal;
+
+import com.app.core.util.JwtUtil;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 校验 JWT Token
+ */
+@Log4j2
+public class JWTAuthFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader("Authorization");
+
+        if (StringUtils.isBlank(header) || !StringUtils.startsWith(header, "Bearer ")) {
+            log.warn("用户认证失败，请求头中 Authorization 字段不存在或格式错误");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // 从 Token 中获取用户信息
+        String userId = JwtUtil.parseJWT(header).getSubject();
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        if (log.isDebugEnabled())
+            log.debug("[JWT] Token 校验成功，用户编号：{}", userId);
+
+        chain.doFilter(request, response);
+    }
+}
--- mould-web/src/main/java/com/app/web/internal/WebSecurityConfig.java
+++ mould-web/src/main/java/com/app/web/internal/WebSecurityConfig.java
@@ -1,42 +1,38 @@
-//package com.app.web.internal;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-////@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private UserDetailsService userDetailsService;
-//
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-//    public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-//        this.userDetailsService = userDetailsService;
-//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable().authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/login", "/addUser").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilter(new JWTLoginFilter(authenticationManager()))
-//                .addFilter(new JWTAuthFilter(authenticationManager()));
-//    }
-//
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.app.web.internal;
+
+import com.app.core.util.IocUtil;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+@EnableWebSecurity(debug = false)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        // 忽略 Swagger2 请求
+        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html**", "/webjars/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/signIn", "/signUp").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().and()
+                .addFilter(new JWTLoginFilter(authenticationManager()))
+                .addFilter(new JWTAuthFilter(authenticationManager()))
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(IocUtil.getBean(UserDetailsService.class))
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+    }
+}
