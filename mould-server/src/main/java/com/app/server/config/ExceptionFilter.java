f1bb3466971f9a40728cca2a4cd4dc9f4cc6ed79
wjtree <21wjtree@163.com>
2018-09-27 22:52:16 +0800

    添加 Kafka 组件

--- mould-server/src/main/java/com/app/server/config/ExceptionFilter.java
+++ mould-server/src/main/java/com/app/server/config/ExceptionFilter.java
@@ -1,12 +1,10 @@
 package com.app.server.config;
 
+import com.alibaba.dubbo.common.Constants;
+import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 
-/**
- * @Description
- * @author: xiaozhonghseng
- * @create: 2018-09-19 16:24
- **/
+@Activate(group = Constants.PROVIDER)
 public class ExceptionFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
